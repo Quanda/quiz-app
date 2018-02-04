ce77ee06e6ef1d70c35580c18fd084a47b31197a
Eric <eric.furspan@gmail.com>
2018-02-04 15:36:38 -0500

    Minor tweaks

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function handleStartQuiz() {
         $('.intro').remove();
 
         // proceed to the first question
-        $('.quiz').html(populateForm);
+        $('.quiz').html(returnForm);
     })
 }
 // Returns the question form, dependent on which question the user is on
-function populateForm() {
+function returnForm() {
     return `
     <div class="scoreboard">
         <ul>
@@ -54,9 +54,10 @@ function populateForm() {
     
 }
 
-// When user Submits answer, check if it was correct
+// When user Submits answer, first check if a radio was selected
+// if one was, check if it was the correct answer
 // if correct, call userAnswerCorrect, else call userAnswerWrong
-// if there are more questions, proceed to next
+// if there are more questions, proceed to next. otherwise, render the results page
 function userSubmitAnswer() {
     $('.quiz').on('click', '#submitAnswer', function(event) {
         event.preventDefault();
@@ -122,47 +123,54 @@ function incrementScore() {
 function nextQuestion() {
     $('.quiz').on('click', '.nextQuestion', function() {
         // populate the .quiz class with next question form
-        renderFeedback(populateForm());
+        renderFeedback(returnForm());
     })
 }
 // returns final page view
 function returnResults() {
+    let grade;
     let resultsTitle;
     let resultsDescription;
     
     // perfect score
     if (score === questionCount) {
+        grade = `A`
         resultsTitle = `Perfect!`;
         resultsDescription = `You must have a pet bald eagle...`;
     }
     // score is greater than 75% but not perfect
     if(score > (questionCount * .75) && (score < questionCount) ) {
+        grade = `B`
         resultsTitle = `Good job, Patriot!`;
-        resultsDescription = ``;
+        resultsDescription = `I salute you`;
     }
     // score is greater than 50% but less than 75%
     else if(score > (questionCount * .50) && (score < questionCount * .75) ) {
-        resultsTitle = `Not bad!`;
-        resultsDescription = ``;
+        grade = `C`
+        resultsTitle = `Meh, not terrible`;
+        resultsDescription = `Might want to brush up though`;
     }
     // score is less than 50%
     else {
+        grade = `F`
         resultsTitle = `Do you even America?`;
         resultsDescription = `Benjamin Franklin is rolling over in his grave`;
     }
-    return `<div class="feedback results scoreD">
+    return `<div class="feedback results ${grade}">
                 <h2>${resultsTitle}</h2>
-                <p>You scored ${score} / ${questionCount}</p>
+                <p>You scored ${score} / ${questionCount}</p><br>
                 <p>${resultsDescription}</p>
                 <button type="button" class="restart-quiz">Restart Quiz</button>          
-            </div>`
+            </div>
+            <footer>
+            </footer>`
 }
 // reset score and questionCount and render the question form
 function restartQuiz() {
     $('.quiz').on('click', '.restart-quiz', function() {
         score = 0;
         questionCount = 0;
-        renderFeedback(populateForm());
+        renderFeedback(returnForm());
     });
 }
 
