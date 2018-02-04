65c1dee715ec16999cfa9c4dd6247c636c37a442
Eric <eric.furspan@gmail.com>
2018-02-03 21:53:22 -0500

    Added validation to radio inputs and refactored functions

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function handleStartQuiz() {
         $('.intro').remove();
         
         // proceed to the first question
-        $('.quiz').prepend(populateNextQuestionForm);
+        $('.quiz').prepend(populateForm);
     })
 }
 
 // Returns the question form, dependent on which question the user is on
-function populateNextQuestionForm() {
+function populateForm() {
     return `
     <div class="scoreboard">
         <ul>
@@ -36,7 +36,7 @@ function populateNextQuestionForm() {
     <form>
         <fieldset>
             <label class="answerOption">
-            <input type="radio" value="${DATA[questionCount].answers[0]}" name="answer" required>
+            <input type="radio" value="${DATA[questionCount].answers[0]}" name="answer">
                 <span>${DATA[questionCount].answers[0]}</span>
             </label>
             <label class="answerOption">
@@ -54,6 +54,7 @@ function populateNextQuestionForm() {
             <button type="submit" id="submitAnswer">Submit</button>
         </fieldset>
     </form>`
+    
 }
 
 // When user Submits answer, check if it was correct
@@ -62,32 +63,39 @@ function populateNextQuestionForm() {
 function userSubmitAnswer() {
     $('.quiz').on('click', '#submitAnswer', function(event) {
         event.preventDefault();
-        let userChoice = $('input[name=answer]:checked');
-        let answer = userChoice.val();
-        let correctAnswer = `${DATA[questionCount].correctAnswer}`
-        // if the user chose the correct answer
-        if (answer === correctAnswer) {
-            userAnswerCorrect();
+        if($('input[name=answer]:checked').length <= 0) {
+            alert('Please select an answer');
         }
         else {
-            userAnswerWrong(correctAnswer);
-        }
-        questionCount++;
-        if(questionCount !== DATA.length) {
-            proceedToNextQuestion();
-        }
-        else {
-            renderResults();
+           let userChoice = $('input[name=answer]:checked');
+            let answer = userChoice.val();
+            let correctAnswer = `${DATA[questionCount].correctAnswer}`
+
+            // if the user chose the correct answer
+            if (answer === correctAnswer) {
+                userAnswerCorrect();
+            }
+            else {
+                userAnswerWrong(correctAnswer);
+            }
+            questionCount++;
+            if(questionCount !== DATA.length) {
+                nextQuestion();
+            }
+            else {
+                renderFeedback(returnResults());
+            } 
         }
     })
 }
-
+// returns correct answer view
 function returnCorrectFeedback() {
     return `<div class="feedback correct">
                 <h2>Correct!</h2>
                 <button type="button" class="nextQuestion">Next Question</button>          
             </div>`
 }
+// returns wrong answer view
 function returnWrongFeedback(correctAnswer) {
     return `<div class="feedback wrong">
                 <h2>Wrong</h2>
@@ -95,74 +103,66 @@ function returnWrongFeedback(correctAnswer) {
                 <button type="button" class="nextQuestion">Next Question</button>          
             </div>`
 }
+// render the .quiz view
 function renderFeedback(feedback) {
-    let correctAnswer = `${DATA[questionCount].correctAnswer}`
     $('.quiz').html(feedback)
 }
-// runs if the user submitted a correct answer
+// updates the DOM with the "correct feedback" view
+// increments the score
 function userAnswerCorrect() {
     renderFeedback(returnCorrectFeedback());
     incrementScore();
 }
+// updates the DOM with the "wrong feedback" view 
 function userAnswerWrong(correctAnswer) {
     renderFeedback(returnWrongFeedback(correctAnswer));
 }
-
 function incrementScore() {
     score++;
 }
-
-function proceedToNextQuestion() {
+// uses event delegation to attach click event to .nextQuestion
+// onclick, populate the 
+function nextQuestion() {
     $('.quiz').on('click', '.nextQuestion', function() {
-        // populate the .quiz class with 
-        $('.quiz').html(populateNextQuestionForm(questionCount));
+        // populate the .quiz class with next question form
+        renderFeedback(populateForm(questionCount));
     })
 }
-
-function renderResults() {
-    $('.quiz').html(returnResults());
-}
-
+// returns final page view
 function returnResults() {
+    let resultsTitle;
+    let resultsDescription;
+    
     // perfect score
     if (score === questionCount) {
-        return `<div class="feedback results scoreA">
-                    <h2>Perfect!</h2>
-                    <p>You scored ${score} / ${questionCount}</p>
-                    <p>You must have a pet bald eagle</p>
-                </div>`
+        resultsTitle = `Perfect!`;
+        resultsDescription = `You must have a pet bald eagle`;
     }
     // score is greater than 75% but not perfect
     if(score > (questionCount * .75) && (score < questionCount) ) {
-        return `<div class="feedback results scoreB">
-                    <h2>Good Job, Patriot!</h2>
-                    <p>You scored ${score} / ${questionCount}</p>
-                    <p></p>
-                </div>`
+        resultsTitle = `Good job, Patriot!`;
+        resultsDescription = ``;
     }
     // score is greater than 50% but less than 75%
     else if(score > (questionCount * .50) && (score < questionCount * .75) ) {
-        return `<div class="feedback results scoreC">
-                    <h2>Not bad!</h2>
-                    <p>You scored ${score} / ${questionCount}</p>
-                    <p>You must have a pet bald eagle</p>
-                </div>`
+        resultsTitle = `Not bad!`;
+        resultsDescription = ``;
     }
     // score is less than 50%
-    else if(score < (questionCount * .50) ) {
-         return `<div class="feedback results scoreD">
-                    <h2>Do you even America?</h2>
-                    <p>You scored ${score} / ${questionCount}</p>
-                    <p>Benjamin Franklin is rolling over in his grave</p>
-                </div>`
+    else {
+        resultsTitle = `Do you even America?`;
+        resultsDescription = `Benjamin Franklin is rolling over in his grave`;
     }
+    return `<div class="feedback results scoreD">
+                <h2>${resultsTitle}</h2>
+                <p>You scored ${score} / ${questionCount}</p>
+                <p>${resultsDescription}</p>
+            </div>`
 }
-
 
 // run event listener functions
 function createQuiz () {
     handleStartQuiz();
     userSubmitAnswer();
 }
-
 $(createQuiz);
